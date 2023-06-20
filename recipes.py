e175642fee1443d13b0abd42eb56832c4a471120
Reuben Reyes <reuben.a.reyes@gmail.com>
2023-06-19 17:31:45 -0700

    feat(replacer): add some smarter character replacement

--- recipes.py
+++ recipes.py
@@ -1,8 +1,64 @@
 import argparse
 import requests
 import validators
-import re
+import unicodedata
 from bs4 import BeautifulSoup
+
+class Replacer:
+  __unicode_fraction_descriptors = {
+      "ZERO": 0,
+      "ONE": 1,
+      "TWO": 2,
+      "THREE": 3,
+      "FOUR": 4,
+      "FIVE": 5,
+      "SIX": 6,
+      "SEVEN": 7,
+      "EIGHT": 8,
+      "NINE": 9,
+      "HALF": 2,
+      "HALVES": 2,
+      "THIRD": 3,
+      "THIRDS": 3,
+      "QUARTER": 4,
+      "QUARTERS": 4,
+      "FIFTH": 5,
+      "FIFTHS": 5,
+      "SIXTH": 6,
+      "SIXTHS": 6,
+      "SEVENTH": 7,
+      "SEVENTH": 7,
+      "EIGHTH": 8,
+      "EIGHTHS": 8,
+      "NINTH": 9,
+      "NINTHS": 9,
+  }
+
+  @classmethod
+  def __replace_fraction(cls, unicode_name):
+    fraction_parts = unicode_name.split("FRACTION")[1].strip().split(' ')
+
+    return "/".join([str(cls.__unicode_fraction_descriptors[x]) for x in fraction_parts])
+
+  @classmethod
+  def __is_latin(cls, string: str):
+    end_of_latin_unicode_space = int('0x1EFF', 16)
+    return ord(string) <= end_of_latin_unicode_space
+
+  @classmethod
+  def replace(cls, string: str):
+    if cls.__is_latin(string):
+      un = unicodedata.name(string)
+      if "FRACTION" in un:
+        return cls.__replace_fraction(un)
+
+      return string
+
+    return ""
+
+  @classmethod
+  def replace_all(cls, string: str):
+    return "".join([ cls.replace(char) for char in string ]).strip()
 
 def extract_wprm(soup: BeautifulSoup):
   '''
@@ -19,22 +75,19 @@ def extract_wprm(soup: BeautifulSoup):
   [recipe_name] = soup.select(".wprm-recipe-name")
   print(f"# {recipe_name.text}")
 
-  ingredients = soup.find_all("li",{"class": "wprm-recipe-ingredient" })
+  ingredients = soup.find_all("li", {"class": "wprm-recipe-ingredient" })
   if len(ingredients) > 0:
     print("## Ingredients")
-    for i in ingredients:
-      match = re.search(r"([\w]+[\w\s\.]+)", i.text)
-      if match:
-        ingredient = match.group(0)
-        print(f"* {ingredient}")
+    print('\n'.join([f"* {Replacer.replace_all(i.text)}" for i in ingredients]))
 
   print("")
 
-  instructions = soup.find_all("li",{"class": "wprm-recipe-instruction" })
+  instructions = soup.find_all("li", {"class": "wprm-recipe-instruction" })
   if len(instructions) > 0:
     print("## Instructions")
-    for e, i in enumerate(instructions):
-      print(f"{e + 1}. {i.text.strip()}")
+    print('\n'.join([
+      f"{e+1}. {Replacer.replace_all(i.text)}" for e, i in enumerate(instructions)
+    ]))
 
 def init_parser() -> argparse.ArgumentParser:
   parser = argparse.ArgumentParser(
